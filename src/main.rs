4e65b0768a13e219b919c0f632821701881dfcfb
Diego <ioiotinez@gmail.com>
2021-09-03 20:51:06 -0300

    add console weight on mars

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    println!("Put yor weight: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+    let mut mars_weight: f32 = calculate_weight_on_mars(100.0);
+    mars_weight = mars_weight * 1000.0;
+
+   println!("Weight on Mars: {}kg", mars_weight);
+}
+
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    return (weight / 9.81) * 3.711;
+}
