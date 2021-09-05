63bbd360f126c48373e18642c48e143875b5c35b
Diego <ioiotinez@gmail.com>
2021-09-05 20:03:33 -0300

    finished touches

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::io;
 
 fn main() {
-    println!("Put yor weight: ");
+    println!("Put yor weight (kg): ");
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
-    let mut mars_weight: f32 = calculate_weight_on_mars(100.0);
+    io::stdin().read_line(&mut input).unwrap();
+    let weight: f32 = input.trim().parse().unwrap();
+    let mut mars_weight: f32 = calculate_weight_on_mars(weight);
     mars_weight = mars_weight * 1000.0;
 
-   println!("Weight on Mars: {}kg", mars_weight);
+    println!("Weight on Mars: {}kg", mars_weight);
 }
-
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     return (weight / 9.81) * 3.711;
